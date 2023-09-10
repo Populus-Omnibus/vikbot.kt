e1dbcf3310ce86830a80ced0e7f428910a90c30b
voroscsoki <wcsongor02@gmail.com>
2023-09-10 23:44:20 +0200

    Added defaults for nullable config values

--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/BotConfig.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/BotConfig.kt
@@ -51,7 +51,7 @@ data class ServerEntry(
 @Serializable
 data class RoleEntry(
     val roleId: Long,
-    val emoteName: String, //the full name of the emote that will be displayed in the role selector
-    val fullName: String, //custom name for the role, can be different from the role's actual name
-    val description: String, //the description that will be displayed in the role selector
+    val emoteName: String? = null, //the full name of the emote that will be displayed in the role selector
+    val fullName: String? = null, //custom name for the role, can be different from the role's actual name
+    val description: String? = null, //the description that will be displayed in the role selector
 )
