b7f20c78fadaa3ad4e50ce1a47bc129e37c34045
voroscsoki <wcsongor02@gmail.com>
2023-09-11 20:52:40 +0200

    Memory map moved to proper location

--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/api/JDAExtension.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/api/JDAExtension.kt
@@ -1,13 +1,10 @@
 package io.github.populus_omnibus.vikbot.api
 
-import kotlinx.datetime.Instant
 import net.dv8tion.jda.api.entities.channel.attribute.IGuildChannelContainer
 import java.util.*
 import kotlin.reflect.KProperty
 
 fun IGuildChannelContainer.getTextChannelById(channelId: ULong) = getTextChannelById(channelId.toLong())
-
-fun <K, T> createMemory(): MutableMap<K, Pair<Instant, T>> = mutableMapOf<K, Pair<Instant, T>>().synchronized()
 
 // allow threadLocal to be delegate.
 operator fun <T> ThreadLocal<T>.getValue(thisRef: Any?, property: KProperty<*>): T = get()
--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/api/MemoryMap.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/api/MemoryMap.kt
@@ -8,14 +8,19 @@ import net.dv8tion.jda.api.interactions.InteractionHook
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 
-class MaintainEvent<K,T>(val delay: Duration = 15.minutes, val expireFunction: (K, T) -> Unit = {_, _ ->}, val map: MutableMap<K, Pair<Instant, T>>) {
+
+class MaintainEvent<K, T>(
+    val delay: Duration = 15.minutes, val expireFunction: (K, T) -> Unit = { _, _ -> },
+    val map: MutableMap<K, Pair<Instant, T>>
+) {
     fun generateTimerEvent(): () -> Unit = object : () -> Unit {
         var lastMaintained = Clock.System.now()
 
         override fun invoke() {
-                val now = Clock.System.now()
-                lastMaintained = now
-                map.entries.removeIf { (key, pair) -> (pair.first + delay < now).also {
+            val now = Clock.System.now()
+            lastMaintained = now
+            map.entries.removeIf { (key, pair) ->
+                (pair.first + delay < now).also {
                     if (it) expireFunction(key, pair.second)
                 }
             }
@@ -32,12 +37,15 @@ operator fun VikBotHandler.plusAssign(event: MaintainEvent<*, *>) {
     shutdownEvent += event.generateShutdownEvent()
 }
 
-fun <K,T> MutableMap<K, Pair<Instant, T>>.maintainEvent(delay: Duration = 15.minutes, expireFunction: (K, T) -> Unit = {_, _ ->}) =
+fun <K, T> createMemory(): MutableMap<K, Pair<Instant, T>> = mutableMapOf<K, Pair<Instant, T>>().synchronized()
+
+fun <K, T> MutableMap<K, Pair<Instant, T>>.maintainEvent(delay: Duration = 15.minutes, expireFunction: (K, T) -> Unit = { _, _ -> }) =
     MaintainEvent(delay, expireFunction, this)
 
 operator fun <K, T> MutableMap<K, Pair<Instant, T>>.set(key: K, value: T) {
     this[key] = Clock.System.now() to value
 }
+
 operator fun MutableMap<Long, Pair<Instant, Message>>.plusAssign(interaction: InteractionHook) {
     val msg = interaction.retrieveOriginal().complete()
     this[msg.idLong] = Clock.System.now() to msg
