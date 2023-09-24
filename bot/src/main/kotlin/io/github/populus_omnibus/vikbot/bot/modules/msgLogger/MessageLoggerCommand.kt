4f9a64d4c514374476ef350f0054d53890dea3fd
KosmX <kosmx.mc@gmail.com>
2023-09-25 01:58:14 +0200

    Message logger command rework

--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/msgLogger/MessageLoggerCommand.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/msgLogger/MessageLoggerCommand.kt
@@ -1,11 +1,12 @@
 package io.github.populus_omnibus.vikbot.bot.modules.msgLogger
 
-import io.github.populus_omnibus.vikbot.VikBotHandler
 import io.github.populus_omnibus.vikbot.api.annotations.Command
 import io.github.populus_omnibus.vikbot.api.annotations.CommandType
 import io.github.populus_omnibus.vikbot.api.commands.*
-import io.github.populus_omnibus.vikbot.bot.MessageLoggingLevel
+import io.github.populus_omnibus.vikbot.db.MessageLoggingLevel
+import io.github.populus_omnibus.vikbot.db.Servers
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import org.jetbrains.exposed.sql.transactions.transaction
 
 @Command(type = CommandType.SERVER)
 object MessageLoggerCommand : CommandGroup("logger", "Message logger", {adminOnly()}) {
@@ -14,10 +15,11 @@ object MessageLoggerCommand : CommandGroup("logger", "Message logger", {adminOnl
             val channel by option("channel", "channel", SlashOptionType.CHANNEL).required()
 
             override suspend fun invoke(event: SlashCommandInteractionEvent) {
-                val server = VikBotHandler.config.servers[event.guild!!.idLong]
-                server.deletedMessagesChannel = channel.idLong
+                transaction {
+                    val server = Servers[event.guild!!.idLong]
+                    server.deletedMessagesChannel = channel.idLong
+                }
 
-                VikBotHandler.config.save()
                 event.reply("Deleted message logging channel is set to <#${channel.idLong}>.").setEphemeral(true).complete()
             }
         }
@@ -26,16 +28,17 @@ object MessageLoggerCommand : CommandGroup("logger", "Message logger", {adminOnl
             val mode by option("mode", "new mode", ListOptionType.ofEnum<MessageLoggingLevel>()).required()
 
             override suspend fun invoke(event: SlashCommandInteractionEvent) {
-                val server = VikBotHandler.config.servers[event.guild!!.idLong]
-                server.messageLoggingLevel = mode
-                VikBotHandler.config.save()
+                transaction {
+                    val server = Servers[event.guild!!.idLong]
+                    server.messageLoggingLevel = mode
+                }
                 event.reply("Config updated").setEphemeral(true).complete()
             }
         }
 
         this += object : SlashCommand("info", "Get current stats") {
             override suspend fun invoke(event: SlashCommandInteractionEvent) {
-                val currentServer = VikBotHandler.config.servers[event.guild!!.idLong]
+                val currentServer = transaction { Servers[event.guild!!.idLong] }
                 event.reply("""
                     Logger on this server is set to ${currentServer.messageLoggingLevel}
                     Target channel is <#${currentServer.deletedMessagesChannel}>
