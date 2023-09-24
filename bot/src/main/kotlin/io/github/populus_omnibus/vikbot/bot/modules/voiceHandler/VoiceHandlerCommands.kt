74defac8da93a433631b2d567d140c5950554757
KosmX <kosmx.mc@gmail.com>
2023-09-25 01:55:48 +0200

    Rework voice handler

--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/voiceHandler/VoiceHandlerCommands.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/voiceHandler/VoiceHandlerCommands.kt
@@ -1,13 +1,18 @@
 package io.github.populus_omnibus.vikbot.bot.modules.voiceHandler
 
-import io.github.populus_omnibus.vikbot.VikBotHandler
 import io.github.populus_omnibus.vikbot.api.annotations.Command
 import io.github.populus_omnibus.vikbot.api.commands.CommandGroup
 import io.github.populus_omnibus.vikbot.api.commands.SlashCommand
 import io.github.populus_omnibus.vikbot.api.commands.SlashOptionType
 import io.github.populus_omnibus.vikbot.api.commands.adminOnly
+import io.github.populus_omnibus.vikbot.db.HandledVoiceChannel
+import io.github.populus_omnibus.vikbot.db.HandledVoiceChannels
+import io.github.populus_omnibus.vikbot.db.Servers
+import io.github.populus_omnibus.vikbot.db.VoiceChannelType
 import kotlinx.coroutines.coroutineScope
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.transactions.transaction
 
 
 @Command
@@ -19,12 +24,16 @@ object VoiceHandlerCommands : CommandGroup("voice", "manage voice channel manage
             override suspend fun invoke(event: SlashCommandInteractionEvent): Unit = coroutineScope {
                 try {
                     val voiceChannel = channel.asVoiceChannel().idLong
-                    if (voiceChannel in VikBotHandler.config.servers[event.guild!!.idLong].handledVoiceChannels) {
-                        event.reply("Channel is already managed")
-                    } else {
-                        VikBotHandler.config.servers[event.guild!!.idLong].handledVoiceChannels += voiceChannel
-                        VikBotHandler.config.save()
-                        event.reply("Channel added to managed channels")
+                    transaction {
+                        if (HandledVoiceChannel.findById(voiceChannel)?.type == VoiceChannelType.VoiceRequest) {
+                            event.reply("Channel is already managed")
+                        } else {
+                            HandledVoiceChannel.new(voiceChannel) {
+                                type = VoiceChannelType.VoiceRequest
+                                guild = Servers[event.guild!!.idLong]
+                            }
+                            event.reply("Channel added to managed channels")
+                        }
                     }
                 } catch (e: IllegalStateException) {
                     event.reply("Channel is not a voice channel")
@@ -36,24 +45,27 @@ object VoiceHandlerCommands : CommandGroup("voice", "manage voice channel manage
             val channel by option("channel", "Voice channel to stop being managed", SlashOptionType.CHANNEL).required()
 
             override suspend fun invoke(event: SlashCommandInteractionEvent) {
-                if (channel.idLong in VikBotHandler.config.servers[event.guild!!.idLong].handledVoiceChannels) {
-                    VikBotHandler.config.servers[event.guild!!.idLong].handledVoiceChannels -= channel.idLong
-                    VikBotHandler.config.save()
-                    event.reply("Channel is no longer managed")
-                } else {
-                    event.reply("Channel was not managed voice channel")
+                transaction {
+                    val ch = HandledVoiceChannel.find {
+                        HandledVoiceChannels.channel eq channel.idLong and (HandledVoiceChannels.channelType eq VoiceChannelType.Temp)
+                    }.firstOrNull()
+                    if (ch != null) {
+                        ch.delete()
+                        event.reply("Channel is no longer managed")
+                    } else {
+                        event.reply("Channel was not managed voice channel")
+                    }
                 }.setEphemeral(true).complete()
             }
         }
 
         this += object : SlashCommand("listChannels".lowercase(), "List managed voice channels") {
             override suspend fun invoke(event: SlashCommandInteractionEvent): Unit = coroutineScope {
-                val managedChannels = VikBotHandler.config.servers[event.guild!!.idLong].handledVoiceChannels
-
-                if (managedChannels.isEmpty()) {
+                val managedChannels = transaction { Servers[event.guild!!.idLong].handledVoiceChannels }
+                if (managedChannels.empty()) {
                     event.reply("There are no managed voice channel on this server")
                 } else {
-                    val channelList = managedChannels.joinToString("\n") { "- <#$it>" }
+                    val channelList = managedChannels.joinToString("\n") { "- <#$it>  ${it.type}" }
                     event.reply("Managed voice channels:\n$channelList")
 
                 }.setEphemeral(true).complete()
--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/voiceHandler/VoiceHandlerModule.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/voiceHandler/VoiceHandlerModule.kt
@@ -3,45 +3,59 @@ package io.github.populus_omnibus.vikbot.bot.modules.voiceHandler
 import io.github.populus_omnibus.vikbot.VikBotHandler
 import io.github.populus_omnibus.vikbot.api.EventResult
 import io.github.populus_omnibus.vikbot.api.annotations.Module
+import io.github.populus_omnibus.vikbot.db.HandledVoiceChannel
+import io.github.populus_omnibus.vikbot.db.HandledVoiceChannels
+import io.github.populus_omnibus.vikbot.db.VoiceChannelType
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel
-import java.util.*
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.transactions.transaction
 import kotlin.time.Duration.Companion.seconds
 
 
 object VoiceHandlerModule {
-
-    private val activeChannels: MutableSet<Long> = Collections.synchronizedSet(mutableSetOf<Long>())
-
     @Module
     fun initModule(bot: VikBotHandler) {
         bot.guildVoiceUpdateEvent[64] = { event ->
             coroutineScope {
-                val voiceChannels = VikBotHandler.config.servers[event.guild.idLong].handledVoiceChannels
 
+                // launch to create new channel
                 launch {
-                    if (event.channelJoined?.idLong in voiceChannels) {
-                        val newChannel =
-                            event.guild.createVoiceChannel("${event.member.effectiveName} által kért voice").apply {
-                                this.setParent(event.channelJoined!!.parentCategory)
-                                this.setPosition(event.channelJoined!!.position + 1)
-                            }.complete()
+                    transaction {
+                        val chId = event.channelJoined?.idLong?.let {
+                            HandledVoiceChannel.find { (HandledVoiceChannels.channel eq it) and (HandledVoiceChannels.channelType eq VoiceChannelType.VoiceRequest) }.firstOrNull()
+                        }
+                        if (chId != null) {
+                            val newChannel =
+                                event.guild.createVoiceChannel("${event.member.effectiveName} által kért voice").apply {
+                                    this.setParent(event.channelJoined!!.parentCategory)
+                                    this.setPosition(event.channelJoined!!.position + 1)
+                                }.complete()
 
-                        activeChannels += newChannel.idLong
-                        event.guild.moveVoiceMember(event.member, newChannel).complete()
+                            HandledVoiceChannel.new(newChannel.idLong) {
+                                type = VoiceChannelType.Temp
+                            }
+                            event.guild.moveVoiceMember(event.member, newChannel).complete()
+                        }
                     }
                 }
                 launch {
-                    if (event.channelLeft?.idLong in activeChannels) {
-                        delay(5.seconds)
-                        val chId = event.channelLeft!!.idLong
-                        val channel = event.guild.getChannelById(VoiceChannel::class.java, chId)
-                        if (channel != null && channel.members.isEmpty()) {
-                            channel.delete().complete()
+                        val tmpChannel = event.channelLeft?.idLong?.let {
+                            transaction { HandledVoiceChannel.find { HandledVoiceChannels.channel eq it and (HandledVoiceChannels.channelType eq VoiceChannelType.Temp) }.firstOrNull() }
                         }
-                    }
+                        if (tmpChannel != null) {
+                            delay(5.seconds)
+                            val chId = event.channelLeft!!.idLong
+                            val channel = event.guild.getChannelById(VoiceChannel::class.java, chId)
+                            if (channel != null && channel.members.isEmpty()) {
+                                channel.delete().complete()
+                            }
+                            transaction {
+                                tmpChannel.delete()
+                            }
+                        }
                 }
             }
 
