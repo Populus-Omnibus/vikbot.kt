b9a856fc4d8b383d8da307fb19572d1de0b58e5f
KosmX <kosmx.mc@gmail.com>
2023-09-24 22:01:50 +0200

    Remove DB content from JSON config

--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/BotConfig.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/BotConfig.kt
@@ -1,12 +1,8 @@
 package io.github.populus_omnibus.vikbot.bot
 
-import io.github.populus_omnibus.vikbot.api.DefaultMap
-import io.github.populus_omnibus.vikbot.api.synchronized
-import io.github.populus_omnibus.vikbot.bot.RoleGroup.PublishData
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.internal.synchronized
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 import kotlinx.serialization.json.Json
@@ -23,15 +19,11 @@ data class BotConfig(
     //var mailChannel: Long,
     val embedColor: Int = 0x03FCC2, //HEX VALUE
     val adminId: Long,
-    @SerialName("serverEntries")
-    private val seMap: MutableMap<Long, ServerEntry> = mutableMapOf<Long,ServerEntry>().synchronized(),
     val vikAuthPort: Int = 12345,
     val vikAuthFernet: String,
     val useRoleTags: Boolean = true,
     val database: DatabaseAccess = DatabaseAccess(),
 ) {
-    val servers: DefaultMap<Long, ServerEntry>
-        get() = DefaultMap(seMap) { ServerEntry() }
 
     @Transient
     private val json = Json {
@@ -55,44 +47,3 @@ data class DatabaseAccess(
     val username: String = "",
     val password: String = "",
 )
-
-@Serializable
-data class ServerEntry(
-    var newsChannel: Long? = null,
-    var reportChannel: Long? = null,
-    var deletedMessagesChannel: Long? = null,
-    var lastRoleResetMessage: PublishData? = null,
-    var messageLoggingLevel: MessageLoggingLevel = MessageLoggingLevel.NONE,
-    val handledVoiceChannels: MutableList<Long> = mutableListOf(),
-    var rssFeeds: MutableList<String> = mutableListOf(),
-    @SerialName("roleGroups")
-    private val rgMap: MutableMap<String, RoleGroup> = mutableMapOf<String, RoleGroup>().synchronized(), //second is the group in which the role is
-){
-    val roleGroups: DefaultMap<String, RoleGroup>
-        get() = DefaultMap(rgMap) { RoleGroup() }
-}
-
-@Serializable
-data class RoleGroup(
-    val roles: MutableList<RoleEntry> = mutableListOf(),
-    var maxRolesAllowed: Int = 25,
-    var lastPublished: PublishData? = null,
-    var genericRoleId: Long? = null
-) {
-    @Serializable
-    data class PublishData(val channelId: Long, val messageId: Long)
-
-    @Serializable
-    data class RoleEntry(
-        val roleId: Long,
-        var descriptor: RoleDescriptor
-    ) {
-        @Serializable
-        data class RoleDescriptor(
-            val emoteName: String, //the full name of the emote that will be displayed in the role selector
-            val apiName: String,
-            val fullName: String, //custom name for the role, can be different from the role's actual name
-            val description: String, //the description that will be displayed in the role selector
-        )
-    }
-}
