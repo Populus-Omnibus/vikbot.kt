2dbcdaefaa797fb801408891a2a2a1aa9987d939
voroscsoki <wcsongor02@gmail.com>
2023-09-24 18:53:08 +0200

    Formatting, check output channel

--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/report/ReportForm.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/report/ReportForm.kt
@@ -27,16 +27,18 @@ import kotlin.time.Duration.Companion.minutes
 
 object ReportForm : ListenerAdapter() {
     private const val APP_NAME = "Report message"
+
     //map to store message data the moment the report form is fired
     private val fallbackMsgData = createMemory<Long, Message>()
 
     @Module
-    fun init(bot: VikBotHandler){
+    fun init(bot: VikBotHandler) {
         val reportFormCommand = Commands.context(Command.Type.MESSAGE, APP_NAME)
 
         bot += fallbackMsgData.maintainEvent(15.minutes)
 
-        bot.serverCommands += object : SlashCommand("setreportchannel","sets the channel to use in this guild for receiving reports", {adminOnly()}){
+        bot.serverCommands += object :
+            SlashCommand("setreportchannel", "sets the channel to use in this guild for receiving reports", { adminOnly() }) {
             val channel by option("channel", "target channel, not supplying this option will cause a reset", SlashOptionType.CHANNEL)
 
             override suspend fun invoke(event: SlashCommandInteractionEvent) {
@@ -50,19 +52,23 @@ object ReportForm : ListenerAdapter() {
         bot.guildInitEvent += {
             it.guild.upsertCommand(reportFormCommand).complete()
         }
+
         bot.messageContextInteractionEvent[64] = { event ->
             if (event.name == APP_NAME) {
                 val modal = Modal.create("reportform:${event.target.idLong}", "Report message")
-                        .addActionRow(TextInput.create("reportReason", "Information about report (can be empty)", TextInputStyle.PARAGRAPH)
-                        .setRequired(false).build()).build()
+                    .addActionRow(TextInput.create("reportReason", "Information about report (can be empty)", TextInputStyle.PARAGRAPH)
+                        .setRequired(false).build()
+                    ).build()
                 fallbackMsgData += event.target
                 event.replyModal(modal).complete()
             }
             EventResult.PASS
         }
-        bot.modalEvents += IdentifiableInteractionHandler("reportform"){ event ->
+
+        bot.modalEvents += IdentifiableInteractionHandler("reportform") { event ->
             event.deferReply().setEphemeral(true).complete()
-            val channel = bot.config.servers[event.guild?.idLong]?.reportChannel?.let { id -> bot.jda.getTextChannelById(id) }
+            val channel =
+                bot.config.servers[event.guild?.idLong]?.reportChannel?.let { id -> bot.jda.getTextChannelById(id) }
             val msgId = event.modalId.split(":").getOrNull(1)?.toLongOrNull()
             msgId?.let { id ->
                 val reportedMessage = try {
@@ -76,7 +82,13 @@ object ReportForm : ListenerAdapter() {
                     val reporter = event.user
                     val reason = event.getValue("reportReason")?.asString?.takeIf { it.isNotEmpty() } ?: "<no reason>"
 
-                    channel?.sendMessageEmbeds(EmbedBuilder().apply {
+                    if(channel == null)
+                    {
+                        event.hook.editOriginal("no output channel set, notify the admins!").complete()
+                        return@IdentifiableInteractionHandler
+                    }
+
+                    channel.sendMessageEmbeds(EmbedBuilder().apply {
                         setColor(bot.config.embedColor)
                         setAuthor(reportedUser.effectiveName, null, reportedUser.avatarUrl)
                         setDescription("**${reportedUser.idLong.toUserTag()} had their [message](${message.jumpUrl}) reported**")
@@ -88,14 +100,13 @@ object ReportForm : ListenerAdapter() {
                         reportedMessage.attachments.filter { it.isImage }.getOrNull(0)?.let {
                             setImage(it.url) //if the message contained an image, store that
                         }
-                    }.build())?.complete()
+                    }.build()).complete()
 
                     event.hook.editOriginal("done!").complete()
                 }
             } ?: run {
                 event.hook.editOriginal("failed, contact admins if your request should've gone through").complete()
             }
-            //if no channel is set in the config file, the report will fall on deaf ears
         }
     }
 }
