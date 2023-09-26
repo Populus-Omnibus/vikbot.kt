fec60bd53231d0092a8dd0f60c5afd8e563deb1e
KosmX <kosmx.mc@gmail.com>
2023-09-26 22:32:02 +0200

    [db] some sqlite tweaks

--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/db/DatabaseHandler.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/db/DatabaseHandler.kt
@@ -7,18 +7,20 @@ import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.slf4j.kotlin.getLogger
 import org.slf4j.kotlin.warn
+import java.sql.Connection
 
 object DatabaseHandler {
 
     private val logger by getLogger()
 
     fun loadDatabase(config: DatabaseAccess) {
-        if (config.driver == "org.sqlite.JDBC") {
+        val isSQLite = config.driver == "org.sqlite.JDBC"
+        if (isSQLite) {
             logger.warn { "SQLite database backend is not recommended in production, consider using a proper database like MariaDB" }
         }
 
         TransactionManager.defaultDatabase =
-            Database.connect(config.address, config.driver, config.password, config.password).also {
+            Database.connect(config.address, config.driver, config.username, config.password).also {
                 transaction(it) {
 
                     // Auto-create/update the actual database
@@ -34,5 +36,9 @@ object DatabaseHandler {
                 }
             }
 
+        if (isSQLite) {
+            TransactionManager.manager.defaultIsolationLevel =
+                Connection.TRANSACTION_SERIALIZABLE
+        }
     }
 }
