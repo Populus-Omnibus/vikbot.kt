61afc4c5f6087d54f712f763639d43b955fcf5ec
KosmX <kosmx.mc@gmail.com>
2023-10-19 01:56:13 +0200

    Tag Utils

---
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/tags/TagUtil.kt
@@ -0,0 +1,59 @@
+package io.github.populus_omnibus.vikbot.bot.modules.tags
+
+import io.github.populus_omnibus.vikbot.api.commands.SlashOptionType
+import io.github.populus_omnibus.vikbot.db.Tag
+import io.github.populus_omnibus.vikbot.db.TagTable
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
+import net.dv8tion.jda.api.interactions.commands.OptionMapping
+import net.dv8tion.jda.api.interactions.commands.OptionType
+import net.dv8tion.jda.api.utils.FileUpload
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder
+import net.dv8tion.jda.api.utils.messages.MessageCreateData
+import org.jetbrains.exposed.sql.LikePattern
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
+internal fun Tag.asMessage(): MessageCreateData {
+    return transaction {
+
+        MessageCreateBuilder().apply {
+            setContent(content)
+            setFiles(
+                this@asMessage.embeds.map {
+                    FileUpload.fromData(
+                        it.data.inputStream,
+                        it.embedName
+                    )
+                }.toList()
+            )
+        }.build()
+    }
+}
+
+
+internal object TagSlashOption : SlashOptionType<Tag> {
+    override val type: OptionType
+        get() = OptionType.STRING
+    override val optionMapping: (OptionMapping) -> Tag
+        get() = {mapping ->
+            transaction { Tag[mapping.asString] }
+        }
+
+    override val isAutoComplete: Boolean
+        get() = true
+
+    override suspend fun autoCompleteAction(event: CommandAutoCompleteInteractionEvent) {
+        // just some casual db injection stuff...
+        val userString = event.focusedOption.value.replace("\\", "\\\\").replace("_", "\\_").replace("%", "\\%")
+        val ids = transaction {
+             TagTable.slice(TagTable.id).select {
+                 TagTable.id like LikePattern("${userString}%", '\\')
+             }
+                 .limit(25).map { it[TagTable.id].value }.toList()
+        }
+
+        event.replyChoiceStrings(ids).complete()
+
+    }
+}
