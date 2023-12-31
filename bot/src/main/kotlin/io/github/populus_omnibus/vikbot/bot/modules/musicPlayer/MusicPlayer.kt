ae9e610b8c19bb128f1bbd1b4c8fe902d1e56686
voroscsoki <wcsongor02@gmail.com>
2023-12-31 17:03:53 +0100

    Test command (join-leave)

--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/musicPlayer/MusicPlayer.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/musicPlayer/MusicPlayer.kt
@@ -1,12 +1,39 @@
 package io.github.populus_omnibus.vikbot.bot.modules.musicPlayer
 
-import net.dv8tion.jda.api.entities.channel.concrete.VoiceChannel
+import kotlinx.coroutines.sync.withLock
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.channel.unions.AudioChannelUnion
 import java.util.*
 
 class MusicPlayer(
-    val channel: VoiceChannel,
+    var guild: Guild,
+    var channel: AudioChannelUnion? = null,
+    var currentTrack: MusicTrack? = null,
     val playlist: MutableList<MusicTrack> = mutableListOf(),
-    val currentTrack: MusicTrack? = null,
     val timer: Timer = Timer(),
 ) {
+
+    private val mutex = kotlinx.coroutines.sync.Mutex()
+    init {
+        guild.audioManager.connectTimeout = 1000L
+    }
+
+    private suspend fun joinNow() {
+        mutex.withLock {
+            channel?.let {
+                guild.audioManager.openAudioConnection(it)
+            }
+        }
+    }
+    suspend fun joinNow(channel: AudioChannelUnion?) {
+        mutex.withLock {
+            this.channel = channel
+        }
+        joinNow()
+    }
+    suspend fun leave() {
+        mutex.withLock {
+            guild.audioManager.closeAudioConnection()
+        }
+    }
 }
--- bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/musicPlayer/MusicPlayerCommands.kt
+++ bot/src/main/kotlin/io/github/populus_omnibus/vikbot/bot/modules/musicPlayer/MusicPlayerCommands.kt
@@ -4,16 +4,16 @@ import io.github.populus_omnibus.vikbot.VikBotHandler
 import io.github.populus_omnibus.vikbot.api.annotations.Module
 import io.github.populus_omnibus.vikbot.api.commands.CommandGroup
 import io.github.populus_omnibus.vikbot.api.commands.SlashCommand
-import io.github.populus_omnibus.vikbot.api.createMemory
 import kotlinx.coroutines.coroutineScope
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import org.slf4j.kotlin.getLogger
+import kotlin.collections.set
 
 //@Command(type = CommandType.SERVER)
 //replaced by init statements
-object MusicPlayerCommands : CommandGroup("music", "Music player", {}) {
+object MusicPlayerCommands : CommandGroup("music", "Music player") {
     internal val logger by getLogger()
-    private var playerInstances = createMemory<ULong, MusicPlayer?>()
+    private val playerInstances: MutableMap<Long, MusicPlayer> = mutableMapOf()
 
     @Module
     fun init(bot: VikBotHandler) {
@@ -22,21 +22,39 @@ object MusicPlayerCommands : CommandGroup("music", "Music player", {}) {
             return
         }
         this += object :
-            SlashCommand("test", "lorem ipsum") {
+            SlashCommand("test", "joins a channel, then leaves") {
 
             override suspend fun invoke(event: SlashCommandInteractionEvent) = coroutineScope {
-
+                event.deferReply().setEphemeral(true).complete()
+                val player = playerInstances[event.guild!!.idLong] ?: MusicPlayer(event.guild!!)
+                player.channel?.let {
+                    event.hook.editOriginal("Already in channel <#${it.idLong}>").complete()
+                    return@coroutineScope
+                }
+                val channel = event.member!!.voiceState!!.channel
+                if (channel == null) {
+                    event.hook.editOriginal("You must be in a voice channel to use this command").complete()
+                    return@coroutineScope
+                }
+                player.joinNow(channel)
+                event.hook.editOriginal("Joined channel <#${channel.idLong}>").complete()
+                Thread.sleep(2000)
+                player.leave()
             }
+        }
+        bot.guildInitEvent += {
+            playerInstances[it.guild.idLong] = MusicPlayer(it.guild)
         }
         bot.serverCommands += this
     }
 
     //check if yt-dlp and ffmpeg are installed
     //the music player module requires both, we're effectively denying user access
-    fun dependencyCheck(): Boolean {
+    private fun dependencyCheck(): Boolean {
         return try {
-            ProcessBuilder("ffmpeg").start().waitFor() == 0
-                    && ProcessBuilder("yt-dlp").start().waitFor() == 0
+            //check if ffmpeg and yt-dlp correspond to a valid OS command
+            ProcessBuilder("ffmpeg", "-version").start().waitFor() == 0 &&
+                    ProcessBuilder("yt-dlp", "--version").start().waitFor() == 0
         } catch (t: Throwable) {
             false
         }
