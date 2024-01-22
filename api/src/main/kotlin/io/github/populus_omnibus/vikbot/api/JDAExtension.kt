cb350248a56a7d7408bc66bc147f0a57a0eb4555
KosmX <kosmx.mc@gmail.com>
2024-01-22 23:47:24 +0100

    Fix JDA dependency

--- api/src/main/kotlin/io/github/populus_omnibus/vikbot/api/JDAExtension.kt
+++ api/src/main/kotlin/io/github/populus_omnibus/vikbot/api/JDAExtension.kt
@@ -1,17 +1,11 @@
 package io.github.populus_omnibus.vikbot.api
 
-import kotlinx.datetime.Instant
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toKotlinInstant
-import kotlinx.datetime.toLocalDateTime
 import net.dv8tion.jda.api.entities.User
 import net.dv8tion.jda.api.entities.channel.attribute.IGuildChannelContainer
-import java.time.OffsetDateTime
-import java.time.format.TextStyle
 import java.util.*
 import kotlin.reflect.KProperty
 
-fun IGuildChannelContainer.getTextChannelById(channelId: ULong) = getTextChannelById(channelId.toLong())
+fun IGuildChannelContainer<*>.getTextChannelById(channelId: ULong) = getTextChannelById(channelId.toLong())
 
 // allow threadLocal to be delegate.
 operator fun <T> ThreadLocal<T>.getValue(thisRef: Any?, property: KProperty<*>): T = get()
